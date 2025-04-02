452ce7d5c4779dfe3b8ad6b9b696247d55877f87
Raspunt <m_ryabinin@inbox.ru>
2025-04-03 00:03:38 +0500

    prisma sqlite + nuxt3 + tailwindcss

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
+
+
+
+
+<style>
+
+
+
+html,body{
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+     <div class="bg-red-500 text-white p-4">Test</div>
+
+</template>
+
+
+<script lang="ts">
+
+
+</script>
+
+
+<style>
+
+
+</style>
+
